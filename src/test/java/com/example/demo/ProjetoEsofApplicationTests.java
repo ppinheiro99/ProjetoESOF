56338863e40d1205e55b0e08967f9c2a287e562c
samuelmmota <71648493+samuelmmota@users.noreply.github.com>
2020-11-12 16:50:01 +0000

    commit test

--- src/test/java/com/example/demo/ProjetoEsofApplicationTests.java
+++ src/test/java/com/example/demo/ProjetoEsofApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ProjetoEsofApplicationTests {
 
     @Test
