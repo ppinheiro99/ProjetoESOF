0aa8c681cc509a320c90d3ca41819a8c4676547f
samuelmmota <71648493+samuelmmota@users.noreply.github.com>
2020-11-12 17:22:58 +0000

    teste1

--- src/test/java/pt/ufp/info/esof/projeto/models/FuncionarioTest.java
+++ src/test/java/pt/ufp/info/esof/projeto/models/FuncionarioTest.java
@@ -1,9 +1,11 @@
 package pt.ufp.info.esof.projeto.models;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class FuncionarioTest {
 
     @Test
